2669c1414543a585b6263656b84ee9b816851fec
Jarek Miszczak <jarek@miszczak.eu>
2023-05-24 08:35:36 +0200

    Improved script for postprocessing data for entropy analysis

---
+++ configurations/postprocess_dat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Note: skeleton for the post processing script
+# World size and initial population are fixed to match included examples
+datFile='analyze.dat'
+echo 'sync,rand,p,st,mutent'
+cat $datFile | sed 's/world_size32_init50//g' | \
+  sed 's/_sync/1,/' | \
+  sed 's/_async/0,/' | \
+  sed 's/_rand_p/1,/g' | \
+  sed 's/_st/,/g' | \
+  sed 's/:/,/g'
+
